48b9add968ad03eaae2649d0b99e93a2c8d8bdde
bibhash15 <bibhash8088@gmail.com>
2026-08-02 23:09:34 +0530

    Time: 1 ms (98.57%), Space: 42.5 MB (83.13%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int sum=0;
+        while(n>0)
+        {
+            int r = n%10;
+            n /= 10;
+            sum += r;
+        }
+        return sum;
+    }
+}
