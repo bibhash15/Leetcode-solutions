801019ad124589112ce7db9d4b26974bad510b22
bibhash15 <bibhash8088@gmail.com>
2026-08-09 23:47:11 +0530

    Time: 9 ms (22.12%), Space: 46.9 MB (13.04%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseWords(String s) {
+        
+        String[] arr = s.split(" ");
+        String ans = "";
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (!arr[i].equals("")) {
+                ans += arr[i] + " ";
+            }
+        }
+
+        return ans.trim();
+    }
+}
